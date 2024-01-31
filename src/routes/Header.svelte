519ceccd1bc07f553ced073249e34eb3496efb24
mojafa <jmohsa@gmail.com>
2024-01-31 17:02:01 +0800

    modified header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
+	import linkedin from '$lib/images/linkedin.svg';
 </script>
 
 <header>
@@ -15,6 +16,7 @@
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
+		<div>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
@@ -22,26 +24,43 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/readings' ? 'page' : undefined}>
+				<a href="/about">Books</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/projects' ? 'page' : undefined}>
+				<a href="/about">Projects</a>
+			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
 			</li>
+			<li aria-current={$page.url.pathname.startsWith('/linkedin') ? 'page' : undefined}>
+				<div class="corner">
+				<a href="https://www.linkedin.com/in/jaafar-mohammed/">
+					<img src={linkedin} alt="LinkedIn" />
+				</a>
+				</div>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/github') ? 'page' : undefined}>
+				<div class="corner">
+		
+					<a href="https://github.com/mojafa">
+						<img src={github} alt="GitHub" />
+					</a>
+				</div>
+			</li>
 		</ul>
+	</div>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
 	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
 	}
 
 	.corner {
@@ -50,7 +69,7 @@
 	}
 
 	.corner a {
-		display: flex;
+		/* display: flex; */
 		align-items: center;
 		justify-content: center;
 		width: 100%;
